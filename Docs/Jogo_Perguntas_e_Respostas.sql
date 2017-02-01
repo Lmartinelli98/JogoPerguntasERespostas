6bdefb6418e63fdf644b83122ae03cf306101ac6
Lmartinelli98 <leoluizmartinelli18@gmail.com>
2017-02-01 17:04:24 -0200

    atualizacao do banco de dados

--- Docs/Jogo_Perguntas_e_Respostas.sql
+++ Docs/Jogo_Perguntas_e_Respostas.sql
@@ -11,3 +11,29 @@ select * from tb_Jogador
 
 insert into tb_Jogador(nome) values('Amanda')
 insert into tb_Jogador(nome) values('Fernanda')
+
+create table tb_Perguntas(
+    id int identity primary key,
+    pergunta varchar(max),
+    resposta_correta varchar(max),
+    data    datetime default getdate()
+    )
+    
+    select * from tb_Perguntas
+    
+    insert into tb_Perguntas(pergunta, resposta_correta)
+    values('Quanto é 4 + 3?','7')
+    
+    select * from tb_Jogador
+    
+    --deleta um id especifico na tabela--
+    delete from tb_Jogador where id = 1
+    
+    --insere um novo id na tabela--
+    insert into tb_Jogador(nome) values('Ramom')
+    
+    --deleta dados da tabela--
+    delete from tb_Jogador
+    
+    --limpa tabela e zera o campo identity--
+    truncate table tb_Jogador
