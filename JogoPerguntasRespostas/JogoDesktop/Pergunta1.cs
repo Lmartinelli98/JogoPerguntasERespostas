e37d52dc5f2ff190fd6c15fba73962513c5f978d
Lmartinelli98 <leoluizmartinelli18@gmail.com>
2017-02-01 16:18:47 -0200

    configuração do banco de dados, configuração da pergunta 1, efeitos sonoros

--- JogoPerguntasRespostas/JogoDesktop/Pergunta1.cs
+++ JogoPerguntasRespostas/JogoDesktop/Pergunta1.cs
@@ -16,5 +16,16 @@ namespace JogoDesktop
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          if(rdbResposta2.Checked == true)
+            {
+                MessageBox.Show("ACERTOU MIZERAVIII!!");
+            }else
+            {
+                MessageBox.Show("ERROOOOUUUUU!!");
+            }
+        }
     }
 }
